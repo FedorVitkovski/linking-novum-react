5d7ff1e5892755965a0550a29fb0004d93af11ae
TheoVitkovskiy <fvitkovski@mail.de>
2017-12-12 21:17:15 +0100

    Fix the fetching of verses

--- src/components/LinkView/index.js
+++ src/components/LinkView/index.js
@@ -7,13 +7,13 @@ const LinkView = (props) => (
         <LinkForm currVerse={props.currVerse} />
         <h1>Book: {props.currVerse.book} Chapter: {props.currVerse.chapter} Verse: {props.currVerse.verse}</h1>
         <h2 style={{marginBottom: '1em'}}>Links to the following sections: </h2>
-        {props.links.map(link => {
+        {props.links.map((link, i) => {
             console.log(link);
             return link.map((verse, index) => {
                 console.log(index);
                 return (
                     <div key={index}>
-                        {(index === 0) ? <div><h2>Book: {verse.book} Chapter: {verse.chapter}</h2><h3>Start Verse: {props.originLinks[index].startVerseFrom} End Verse: {props.originLinks[index].endVerseFrom}</h3><h4>{props.originLinks[index].description}</h4></div> : <p></p>}
+                        {(index === 0) ? <div><h2>Book: {verse.book} Chapter: {verse.chapter}</h2><h3>Start Verse: {props.originLinks[i].startVerseFrom} End Verse: {props.originLinks[i].endVerseFrom}</h3><h4>{props.originLinks[index].description}</h4></div> : <p></p>}
                         <p><strong>{verse.verseNumber}</strong>{verse.body}</p>
                     </div>
                 )
