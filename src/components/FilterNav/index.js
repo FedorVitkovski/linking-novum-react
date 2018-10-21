f4919b37003d36d22cbb40a2886e8dd4748175a9
FedorVitkovskiy <fedor.vitkovskiy@thetimemaster.de>
2018-10-21 16:39:09 +0200

    - refactor handleSearchChapter for use with the new api

--- src/components/FilterNav/index.js
+++ src/components/FilterNav/index.js
@@ -9,16 +9,25 @@ class FilterNav extends Component {
     handleSearchChapter = (e) => {
         e.preventDefault();
         const form = this.form;
-        form.validateFields((err, { book, chapter }) => {
+        form.validateFields(async (err, { book, chapter }) => {
             if (err) {
                 return;
             }
+
+            try {
+                const bookData = await (await fetch(`${process.env.REACT_APP_API_HOST}/book?name=${book.toLowerCase()}`)).json();
+                const bookId = bookData.docs[0]._id;
     
-            fetch(`${process.env.REACT_APP_API_HOST}/api/verses/${book}/${chapter}`)
-                .then(resp => resp.json())
-                .then(verses => {
-                    this.props.setVerses(verses, book, chapter);
-                });
+                const chapterData = await (await fetch(`${process.env.REACT_APP_API_HOST}/book/${bookId}/chapter?number=${chapter}`)).json();
+                const chapterId = chapterData.docs[0]._id;
+    
+                const verseData = await (await fetch(`${process.env.REACT_APP_API_HOST}/chapter/${chapterId}/verse?$sort=counter`)).json();
+                const verses = verseData.docs;
+
+                this.props.setVerses(verses, book, chapter);
+            } catch (e) {
+                console.log(e);
+            }
 
             form.resetFields();
         });
