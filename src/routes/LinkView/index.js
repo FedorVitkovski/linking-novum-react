73ba46059807d85236d30505ba63330f8ab9a037
TheoVitkovskiy <fvitkovski@mail.de>
2018-11-07 14:27:55 +0100

    - PUSH

--- src/routes/LinkView/index.js
+++ src/routes/LinkView/index.js
@@ -8,7 +8,8 @@ class LinkView extends Component {
     
     state = {
         currSectionsVerses: [],
-        linkedSectionsVerses: []
+        linkedSectionsVerses: [],
+        descriptions: []
     }
 
     componentDidMount() {
@@ -26,8 +27,13 @@ class LinkView extends Component {
         });
 
         const currSectionId = e.target.id;
+
         const links = await (await fetch(`${process.env.REACT_APP_API_HOST}/section/${currSectionId}/link`)).json();
+
+        const descriptions = [];
+
         const linkedSectionsIds = links.map(link => {
+            descriptions.push(link.description);
             if (link.section1 == currSectionId) {
                 return link.section2;
             } else {
@@ -35,22 +41,30 @@ class LinkView extends Component {
             }
         });
 
-        const linkedSections = await (await fetch(`${process.env.REACT_APP_API_HOST}/section?_id=${linkedSectionsIds.join(',')}`)).json();
+        this.setState({
+            descriptions
+        });
 
-        this.setSectionVerses(linkedSections.docs, false);
+        const linkedSections = await (await fetch(`${process.env.REACT_APP_API_HOST}/section?_id=${linkedSectionsIds.join(',')}&$embed=book`)).json();
+        const bookName = linkedSections.docs[0].book.name;
+        this.setSectionVerses(linkedSections.docs, false, bookName);
     }
 
-    setSectionVerses = (sections, current = true) => {
+    setSectionVerses = (sections, current = true, bookName = '') => {
         for (let i = 0; i < sections.length; i++) {
             this.fetchSectionVerses(sections[i]).then(({ docs }) => {
     
-                const filteredVerses = docs
-                    .filter(verse => verse.chapter.book.name == this.props.currBook.name)
-                    .sort((a, b) => a.counter > b.counter ? 1 : -1);
+    
 
                 if (current) {
+                    const filteredVerses = docs
+                        .filter(verse => verse.chapter.book.name == this.props.currBook.name)
+                        .sort((a, b) => a.counter > b.counter ? 1 : -1)
                     this.setState(state => ({ currSectionsVerses : [...state.currSectionsVerses, filteredVerses]}));
                 } else {
+                    const filteredVerses = docs
+                        .filter(verse => verse.chapter.book.name == bookName)
+                        .sort((a, b) => a.counter > b.counter ? 1 : -1)
                     this.setState(state => ({ linkedSectionsVerses : [...state.linkedSectionsVerses, filteredVerses] }));
                 }
             });
@@ -61,14 +75,12 @@ class LinkView extends Component {
         return (
             <div>
                 <Link to="/">Select another verse</Link>
-                <Row style={{ width: '100%' }}>
+                <Row style={{ width: '100%', minWidth: '1200px' }}>
                     <Col span={5} style={{ width: '50%' }}>
                         <Collapse>
                         {this.state.currSectionsVerses.map((section, index) => (
                             <Panel
                                 header={`Book: ${section[0].chapter.book.name} From: ${section[0].chapter.name} To: ${section[section.length - 1].chapter.name}`}
-                                id={this.props.currSections[index]._id}
-                                onClick={this.handleCurrSectionClick}
                             >
                                 <Button
                                     onClick={this.handleCurrSectionClick}
@@ -93,9 +105,7 @@ class LinkView extends Component {
                         <Collapse>
                         {this.state.linkedSectionsVerses.map((section, index) => (
                             <Panel
-                                header={`Book: ${section[0].chapter.book.name} From: ${section[0].chapter.name} To: ${section[section.length - 1].chapter.name}`}
-                                id={this.props.currSections[index]._id}
-                                onClick={this.handleCurrSectionClick}
+                                header={`Book: ${section[0].chapter.book.name} | From: ${section[0].chapter.name} To: ${section[section.length - 1].chapter.name} | "${this.state.descriptions[index]}"`}
                             >
                                 {section.map(verse => {
                                     return (
